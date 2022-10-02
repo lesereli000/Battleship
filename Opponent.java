c363d36f3142908e9d58862bb1633980ce638e55
lesereli000 <43320655+lesereli000@users.noreply.github.com>
2022-10-02 01:05:14 -0400

    break smartGuess method into smaller methods

--- Opponent.java
+++ Opponent.java
@@ -4,9 +4,10 @@ import java.util.Random;
 public class Opponent {
 
     private Random r;
-    private int LHlett, LHnum;
+    private int LHlett, LHnum, LHtries, guessLett, guessNum;
     private ArrayList<Integer[]> prevHits;
-    private boolean prevHit;
+    private boolean prevHit, hit;
+    private char lett;
     private Board comp;
     private Board user;
 
@@ -14,6 +15,8 @@ public class Opponent {
         r = new Random();
         LHlett = 0;
         LHnum = 0;
+        guessLett = 0;
+        guessNum = 0;
         prevHits = new ArrayList<Integer[]>();
         prevHit = false;
         this.comp = comp;
@@ -22,87 +25,104 @@ public class Opponent {
 
     public void compSmartGuess() {
         try {
-            char lett = 'a';
-            int lettInd, num, LHtries = 0;
-            LHlett = (int) LHlett > 97 ? LHlett - 97 : LHlett;
+            lett = 'a';
+            LHtries = 0;
+            
+            chooseGuess();
+            
+            checkDuplicate();
+
+            handleGuess();
+            
+            guessOutput();
+
+        } catch (Exception e) {
+            compSmartGuess();
+        }
+    }
+
+    // determine guesses dased on either a random number, or on previous hits
+    private void chooseGuess(){
+        LHlett = (int) LHlett > 97 ? LHlett - 97 : LHlett;
 
             if (!prevHit) {
-                lettInd = r.nextInt(10);
-                num = r.nextInt(9) + 1;
+                guessLett = r.nextInt(10);
+                guessNum = r.nextInt(9) + 1;
             } else if (r.nextInt(2) == 1) {
-                lettInd = LHlett;
-                num = LHnum - 2 + r.nextInt(3);
+                guessLett = LHlett;
+                guessNum = LHnum - 2 + r.nextInt(3);
                 LHtries = 0;
             } else {
-                lettInd = LHlett - 3 + r.nextInt(3);
-                num = LHnum;
+                guessLett = LHlett - 3 + r.nextInt(3);
+                guessNum = LHnum;
                 LHtries = 0;
             }
 
-            for (int i = 0; i < lettInd; i++) {
+            for (int i = 1; i < guessLett; i++) {
                 lett++;
             }
 
+            LHlett = (int) LHlett > 97 ? LHlett - 97 : LHlett;
+    }
+
+    // prevent duplicate guesses
+    private void checkDuplicate(){
+        while (comp.checkGuess(lett, guessNum)) {
             LHlett = (int) LHlett > 96 ? LHlett - 96 : LHlett;
-            // prevent duplicate guesses
-            while (comp.checkGuess(lett, num)) {
-                LHlett = (int) LHlett > 96 ? LHlett - 96 : LHlett;
-                if (!prevHit) {
-                    lettInd = r.nextInt(10);
-                    num = r.nextInt(10) + 1;
-                } else if (r.nextInt(2) == 1) {
-                    lettInd = LHlett;
-                    num = LHnum - 1 + r.nextInt(3);
-                    LHtries++;
-                } else {
-                    lettInd = LHlett - 1 + r.nextInt(3);
-                    num = LHnum;
-                    LHtries++;
-                }
-
-                if (LHtries > 20) {
-                    if (prevHits.size() == 1) {
-                        prevHit = false;
-                        LHtries = 0;
-                        prevHits.clear();
-                    } else {
-                        prevHits.remove(prevHits.size() - 1);
-                        LHtries = 0;
-                    }
-                }
-
-                lett = 'a';
-                for (int i = 0; i < lettInd; i++) {
-                    lett++;
-                }
-            }
-
-            // check if hit or miss then output accordingly
-            boolean hit = user.checkHit(lett, num);
-            comp.guess(hit, lett, num);
-            System.out.println(LHtries);
-            System.out.println(prevHit);
-            // System.out.println(LHlett);
-            // System.out.println(LHnum);
-            if (hit) {
-                LHlett = (int) lett;
-                LHnum = num;
-                prevHit = true;
-                Integer hitLoc[] = { (int) lett, num };
-                prevHits.add(hitLoc);
-            }
-            if (prevHits.size() != 0) {
-                System.out.println(prevHits.get(prevHits.size() - 1)[0] + " " + prevHits.get(prevHits.size() - 1)[1]);
-            }
-            if (hit) {
-                System.out.println("The computer guessed " + lett + num + ". It was a hit!");
+            if (!prevHit) {
+                guessLett = r.nextInt(10);
+                guessNum = r.nextInt(10) + 1;
+            } else if (r.nextInt(2) == 1) {
+                guessLett = LHlett;
+                guessNum = LHnum - 1 + r.nextInt(3);
+                LHtries++;
             } else {
-                System.out.println("The computer guessed " + lett + num + ". It was a miss!");
+                guessLett = LHlett - 1 + r.nextInt(3);
+                guessNum = LHnum;
+                LHtries++;
             }
 
-            LHlett = (int) LHlett > 96 ? LHlett - 96 : LHlett;
-        } catch (Exception e) {
-            compSmartGuess();
+            if (LHtries > 20) {
+                if (prevHits.size() == 1) {
+                    prevHit = false;
+                    LHtries = 0;
+                    prevHits.clear();
+                } else {
+                    prevHits.remove(prevHits.size() - 1);
+                    LHtries = 0;
+                }
+            }
+
+            lett = 'a';
+            for (int i = 0; i < guessLett; i++) {
+                lett++;
+            }
+        }
+    }
+
+    // check if hit or miss
+    private void handleGuess(){
+        hit = user.checkHit(lett, guessNum);
+        comp.guess(hit, lett, guessNum);
+
+        if (hit) {
+            LHlett = (int) lett;
+            LHnum = guessNum;
+            prevHit = true;
+            Integer hitLoc[] = { (int) lett, guessNum };
+            prevHits.add(hitLoc);
+        }
+    }
+
+    // output according to guess
+    private void guessOutput(){
+        if (prevHits.size() != 0) {
+            System.out.println(prevHits.get(prevHits.size() - 1)[0] + " " + prevHits.get(prevHits.size() - 1)[1]);
+        }
+        if (hit) {
+            System.out.println("The computer guessed " + lett + guessNum + ". It was a hit!");
+        } else {
+            System.out.println("The computer guessed " + lett + guessNum + ". It was a miss!");
         }
     }
 
